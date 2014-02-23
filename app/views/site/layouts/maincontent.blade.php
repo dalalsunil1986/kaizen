a273c5b9c7551259092c6db49102086c8b992ae5
uusa35 <uusa35@gmail.com>
2014-02-23 14:06:45 +0300

    Youtube Channel Adjustument + Connecting Youtube API to get One List (last 4 videos added - automatically updated)

--- app/views/site/layouts/maincontent.blade.php
+++ app/views/site/layouts/maincontent.blade.php
@@ -1,35 +1,24 @@
 @section('maincontent')
-<div id="youtube-1">
+<div id="youtube_main" class="row">
 
-   <!-- <iframe style="width:100%; min-height:250px; height: 350px; max-height: 350px; padding-top: 18px; margin:0px; "
+   <iframe style="width:100%; min-height:250px; height: 350px; max-height: 350px; padding-top: 18px; margin:0px; "
             src="//www.youtube.com/embed/N6KpbdJVWIY?list=PLgziGzRnamoJ4ESrNu5Yhtzqc4j-Mer3_" frameborder="0" allowfullscreen></iframe>
     <script src="https://apis.google.com/js/platform.js"></script>
-    <div class="g-ytsubscribe" data-channel="GoogleDevelopers">-->
+    <div class="g-ytsubscribe" data-channel="GoogleDevelopers"></div>
+</div>
+<div id="youtube_playlist" class="row">
+    <?php
+    $playlistdiv = '';
+    $json = json_decode(file_get_contents('http://gdata.youtube.com/feeds/api/playlists/PLgziGzRnamoJ4ESrNu5Yhtzqc4j-Mer3_?v=2&alt=jsonc'));
 
-        <?php
-        $json = json_decode(file_get_contents('http://gdata.youtube.com/feeds/api/playlists/PLgziGzRnamoJ4ESrNu5Yhtzqc4j-Mer3_?v=2&alt=jsonc'));
-print_r($json);
-        exit;
-        $i =0;
-        $playlistdiv = '';
-
-                if ($i <= 4) {
-
-               // $media = $playlist->children('http://search.yahoo.')
-                echo '<pre>';
-                //print_r($playlist);
-                //print_r($links->href);
-                echo '</pre>';
-                    $playlistdiv .= '<div><img src="http://img.youtube.com/vi/'.$json->data->thumbnail->sqDefault.'/default.jpg"/> <h6>'.$json->data->title.'</h6></div>';
-                $i++;
-                }
-
-        echo $playlistdiv;
-        exit;
-        ?>
-    </div>
-
-
+    for($i=0; $i<= 3; $i++) {
+        $playlistdiv .= '<div class="col-md-3"><a href="'.$json->data->items[$i]->video->player->default.'">
+        <img class="img-thumbnail" src="'.$json->data->items[$i]->video->thumbnail->sqDefault.'"/>
+        <p>'.$json->data->items[$i]->video->title.'</p>
+        </div>';
+    }
+    echo $playlistdiv;
+    ?>
 </div>
 
 <div id="side-instagram">
@@ -41,4 +30,5 @@ print_r($json);
         </div>
     </div>
 </div>
+
 @stop
