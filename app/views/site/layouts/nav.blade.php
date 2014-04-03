cdec4c184715e10efd3eed3de8f0cb774981ab4a
ZaL <z4ls@live.com>
2014-04-03 20:04:12 +0300

    Added active state to nav links

--- app/views/site/layouts/nav.blade.php
+++ app/views/site/layouts/nav.blade.php
@@ -13,11 +13,11 @@ extends('site.layouts.home')
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="{{ (Request::is('/'))? 'active' : '' }}" ><a href="{{ action('EventsController@dashboard') }}">{{ Lang::get('site.nav.home')}}</a></li>
-                <li class="{{ Request::is('event')? 'active' : '' }}"><a href="{{ action('EventsController@index') }}">{{ Lang::get('site.nav.events') }}</a></li>
-                <li class="{{ Request::is('consultancy')? 'active' : '' }}"><a href="{{ action('BlogsController@consultancy') }}">{{ Lang::get('site.nav.consultancies') }}</a></li>
-                <li class="{{ Request::is('/blog')? 'active' : '' }}"><a href="{{ action('BlogsController@index') }}">{{ Lang::get('site.nav.posts') }}</a></li>
-                <li class="{{ Request::is('/contact-us')? 'active' : '' }}"><a href="{{ action('ContactsController@index') }}">{{ Lang::get('site.nav.contactus') }}</a></li>
+                <li class="{{ (Request::is('en') || Request::is('ar') )? 'active' : '' }}" ><a href="{{ action('EventsController@dashboard') }}">{{ Lang::get('site.nav.home')}}</a></li>
+                <li class="{{ (Request::is('en/event') || Request::is('ar/event') )? 'active' : '' }}"><a href="{{ action('EventsController@index') }}">{{ Lang::get('site.nav.events') }}</a></li>
+                <li class="{{ (Request::is('en/consultancy')|| Request::is('ar/consultancy'))? 'active' : '' }}"><a href="{{ action('BlogsController@consultancy') }}">{{ Lang::get('site.nav.consultancies') }}</a></li>
+                <li class="{{ (Request::is('en/blog')|| Request::is('ar/blog'))? 'active' : '' }}"><a href="{{ action('BlogsController@index') }}">{{ Lang::get('site.nav.posts') }}</a></li>
+                <li class="{{ (Request::is('en/contact-us')|| Request::is('ar/contact-us'))? 'active' : '' }}"><a href="{{ action('ContactsController@index') }}">{{ Lang::get('site.nav.contactus') }}</a></li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
