057a0b0125288a6497fbd2fb62e063d77cf0427f
ZaL <z4ls@live.com>
2014-05-22 01:58:55 +0300

    major admin panel bug fix

--- app/views/admin/events/index.blade.php
+++ app/views/admin/events/index.blade.php
@@ -22,23 +22,7 @@
 
 		<tbody>
 			@foreach ($events as $event)
-				<tr>
-                    <td>{{{ $event->id }}}</td>
-<!--					<td>{{{ $event->category->name }}}</td>-->
-<!--					<td>{{{ $event->location->name }}}</td>-->
-<!--					<td>{{{ $event->title }}}</td>-->
-<!--					<td>{{{ $event->date_start }}}</td>-->
-<!--					<td>{{{ $event->date_end }}}</td>-->
-<!--					<td>{{{ $event->address }}}</td>-->
-                    <td>{{{ $event->getHumanCreatedAtAttribute() }}} </td>
-                    <td><a href="{{ URL::action('AdminEventsController@settings',$event->id)}}">Settings</a></td>
-                    <td><a href="{{ URL::action('AdminEventsController@edit', array($event->id), array('class' => 'btn btn-info')) }}">Edit</a></td>
-                    <td>
-                        {{ Form::open(array('method' => 'DELETE', 'action' => array('AdminEventsController@destroy', $event->id))) }}
-                            {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
-                        {{ Form::close() }}
-                    </td>
-				</tr>
+
 			@endforeach
 		</tbody>
 	</table>
