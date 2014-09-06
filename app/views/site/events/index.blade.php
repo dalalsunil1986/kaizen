b42a869ded9b81267bea3bda6636f179b325fef2
ZaL <z4ls@live.com>
2014-09-06 09:23:40 +0300

    last commit

--- app/views/site/events/index.blade.php
+++ app/views/site/events/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <style>
     .padded {
-        padding:0;
-        margin:0 5px 0 5px;
+        padding: 0;
+        margin: 0 5px 0 5px;
     }
 </style>
 
@@ -15,7 +15,7 @@
     <div class="col-sm-2 col-md-2">
         <div id="links">
             @if(count($event->photos))
-            <a href="{{ action('EventsController@show',$event->id) }}" >
+            <a href="{{ action('EventsController@show',$event->id) }}">
                 {{ HTML::image('uploads/thumbnail/'.$event->photos[0]->name.'','image1',array('class'=>'img-responsive img-thumbnail')) }}
             </a>
             @else
@@ -26,14 +26,13 @@
         </div>
     </div>
     <div class="col-sm-10 col-md-10">
-                <span class="event-title">
-                    <a href="event/{{$event->id}}">
-                    {{ $event->title }}
-                    </a>
-                </span>
+        <span class="event-title">
+            <a href="event/{{$event->id}}">
+                {{ $event->title }}
+            </a>
+        </span>
         <p>
             {{ Str::limit($event->description, 150) }}
-
             <a href="event/{{ $event->id}}">{{ Lang::get('site.general.more')}}</a>
         </p>
 
@@ -43,28 +42,29 @@
 <div class="row" style="margin: 9px; ">
 
     <i class="glyphicon glyphicon-user">
-        {{ link_to_action('EventsController@index', $event->author->username,array('search'=>'','author'=>$event->author->id))  }}
+        {{ link_to_action('EventsController@index', $event->author->username,array('search'=>'','author'=>$event->author->id)) }}
         |</i>
     <i class="glyphicon glyphicon-calendar"></i> {{ $event->date_start }} -
-    {{ $event->date_end }} |
+                                                 {{ $event->date_end }} |
 
     <i class="glyphicon glyphicon-globe">
         @if ( App::getLocale() == 'en')
-            <?php $country =  ($event->location->country->name_en) ? $event->location->country->name_en : $event->location->country->name_ar ;?>
-            <?php $category =  ($event->category->name_en) ? $event->category->name_en : $event->category->name_ar ;?>
+        <?php $country = ($event->location->country->name_en) ? $event->location->country->name_en : $event->location->country->name_ar; ?>
+        <?php $category = ($event->category->name_en) ? $event->category->name_en : $event->category->name_ar; ?>
         @else
-            <?php $country =  ($event->location->country->name_ar) ? $event->location->country->name_ar : $event->location->country->name_en ;?>
-            <?php $category =  ($event->category->name_ar) ? $event->category->name_ar : $event->category->name_en ;?>
+        <?php $country = ($event->location->country->name_ar) ? $event->location->country->name_ar : $event->location->country->name_en; ?>
+        <?php $category = ($event->category->name_ar) ? $event->category->name_ar : $event->category->name_en; ?>
         @endif
-        {{ link_to_action('EventsController@index', $country ,array('search'=>'','country'=>$event->location->country->id))  }}
+        {{ link_to_action('EventsController@index', $country ,array('search'=>'','country'=>$event->location->country->id)) }}
         |</i>
-    @if ( App::getLocale() == 'en')
-    <i class="glyphicon glyphicon-tag"></i>&nbsp;&nbsp;
-    {{ link_to_action('EventsController@index', $event->category->name_en,array('search'=>'','category'=>$event->category->id))  }}
-    @else
-    <i class="glyphicon glyphicon-tag"></i>&nbsp;&nbsp;
-    {{ link_to_action('EventsController@index', $category,array('search'=>'','category'=>$event->category->id))  }}
-    @endif</div>
+        @if ( App::getLocale() == 'en')
+        <i class="glyphicon glyphicon-tag"></i>&nbsp;&nbsp;
+        {{ link_to_action('EventsController@index', $event->category->name_en,array('search'=>'','category'=>$event->category->id)) }}
+        @else
+        <i class="glyphicon glyphicon-tag"></i>&nbsp;&nbsp;
+        {{ link_to_action('EventsController@index', $category,array('search'=>'','category'=>$event->category->id)) }}
+        @endif
+</div>
 <hr>
 @endforeach
 <?php echo $events->appends(Request::except('page'))->links(); ?>
