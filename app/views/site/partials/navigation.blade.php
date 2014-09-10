333c497704338c483248c1107019418ed6d3f4e4
Usama Ahmed <uusa35@gmail.com>
2014-09-10 20:43:07 +0300

    email template

--- app/views/site/partials/navigation.blade.php
+++ app/views/site/partials/navigation.blade.php
@@ -17,7 +17,14 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li class="{{ (Request::is('en') || Request::is('ar') || Request::is('/')) ? 'active' : '' }}" ><a href="{{ route('home') }}">{{ Lang::get('site.nav.home')}}</a></li>
-                <li class="{{ (Request::segment('1') == 'event' ? 'active' :  false ) }}"><a href="{{ action('EventsController@index') }}">{{ Lang::get('site.nav.events') }}</a></li>
+                <li class="dropdown {{ (Request::segment('1') == 'event' ? 'active' :  false ) }}">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{ Lang::get('site.nav.events') }}</a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="{{ action('EventsController@index') }}" >{{ Lang::get('site.nav.events') }}</a></li>
+                        <li class="divider"></li>
+                        <li><a href="{{ action('EventsController@index') }}">{{ Lang::get('site.nav.events') }}</a></li>
+                    </ul>
+                </li>
                 <li class="{{ (Request::segment('1') == 'consultancy' ? 'active' :  false ) }}"><a href="{{ action('BlogsController@consultancy') }}">{{ Lang::get('site.nav.consultancies') }}</a></li>
                 <li class="{{ (Request::segment('1') == 'blog' ? 'active' :  false ) }}"><a href="{{ action('BlogsController@index') }}">{{ Lang::get('site.nav.posts') }}</a></li>
                 <li class="{{ (Request::segment('1') == 'contact' ? 'active' :  false ) }}"><a href="{{ action('ContactsController@index') }}">{{ Lang::get('site.nav.contactus') }}</a></li>
