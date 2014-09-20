2f5605d2bd3d3bace047548e964dc4de0cbc1337
Usama Ahmed <uusa35@gmail.com>
2014-09-20 19:14:47 +0300

    cloning repository to online domain

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'kaizen',
+			'username'  => 'kaizen',
+			'password'  => 'kaizen_db_user',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
