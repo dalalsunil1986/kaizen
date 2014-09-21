d687e2262fc6accf0e6f10cdaf660e071e367ef8
ZaL <z4ls@live.com>
2014-09-21 19:51:53 +0300

    added roles, user signup in admin panel

---
+++ src/User/Validators/AdminUpdateValidator.php
@@ -0,0 +1,51 @@
+<?php namespace Acme\User\Validators;
+
+use Acme\Core\Validators\AbstractValidator;
+
+class UpdateValidator extends AbstractValidator {
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+
+    protected $rules = array(
+        'phone'    => 'numeric',
+        'mobile'   => 'required|numeric',
+        'name_en'  => 'required|alpha_num|between:3,40',
+        'name_ar'  => 'required|between:3,40',
+        'password' => 'alpha_num|between:6,12|confirmed',
+    );
+
+    public function __construct($id)
+    {
+        parent::__construct();
+
+        $this->id = $id;
+    }
+
+    /**
+     * Get the prepared input data.
+     *
+     * @return array
+     */
+    public function getInputData()
+    {
+        return array_only($this->inputData, [
+            'name_ar', 'name_en', 'password', 'password_confirmation', 'country_id', 'twitter', 'phone', 'mobile'
+        ]);
+    }
+
+    /**
+     * Remove Password field if empty
+     */
+    public function beforeValidation()
+    {
+        if ( empty($this->inputData['password']) )
+            unset($this->inputData['password']);
+
+    }
+
+
+}
