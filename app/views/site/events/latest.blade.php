1e545afb31ad212fa95226fc888c676efe931d31
ZaL <z4ls@live.com>
2014-11-01 20:21:58 +0300

    tweak lang files

--- app/views/site/events/latest.blade.php
+++ app/views/site/events/latest.blade.php
@@ -1,6 +1,6 @@
 <div id="side-1">
     <div class="panel panel-default">
-        <div class="panel-heading"> {{ trans('word.latest_blog') }} </div>
+        <div class="panel-heading"> {{ trans('word.latest_events') }} </div>
         <div class="panel-body">
             <ul>
                 @if($latest_event_posts)
