eb2e210546af8a547d622ae262d3e7b0000dc8f7
ZaL <z4ls@live.com>
2014-11-07 15:09:42 +0300

    past events

--- app/views/site/events/index.blade.php
+++ app/views/site/events/index.blade.php
@@ -1,33 +1,8 @@
 @extends('site.layouts._two_column')
 
 @section('sidebar')
-    <div class="panel panel-default">
-        <div class="panel-heading">{{ trans('word.expired_events') }}</div>
-        <div class="panel-body">
-            <ul>
-                @if($expiredEvents)
-                    @foreach($expiredEvents as $event)
-                        <li class="unstyled"><i class="glyphicon glyphicon-calendar"></i> <a href="{{URL::action('EventsController@show',$event->id)}}"> {{ $event->title }}</a></li>
-                    @endforeach
-                    <hr>
-                    <a href="{{action('EventsController@index',['past'=>'true'])}}">{{ trans('word.view_all_expired_events') }}</a>
-                @endif
-            </ul>
-        </div>
-    </div>
-
-    <div class="panel panel-default">
-        <div class="panel-heading">{{ trans('word.category') }}</div>
-        <div class="panel-body">
-            <ul>
-                @if($eventCategories)
-                    @foreach($eventCategories as $eventCategory)
-                        <li class="unstyled"><i class="glyphicon glyphicon-tag"></i><a href="{{action('CategoriesController@getEvents',$eventCategory->id)}}"> {{ $eventCategory->name }}</a></li>
-                    @endforeach
-                @endif
-            </ul>
-        </div>
-    </div>
+    @include('site.events._expired')
+    @include('site.events._category')
     @parent
 @stop
 
