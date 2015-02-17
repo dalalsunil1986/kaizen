8dcca8731def1a85c6584651c918cf60c1ce4e36
ZaL <z4ls@live.com>
2015-02-17 19:55:45 +0300

    tweak views

--- app/library/helper.php
+++ app/library/helper.php
@@ -4,31 +4,38 @@
  * Class Helper
  * All Helper functions
  */
-
 class Helper {
-    public static  function isMod() {
-        if (!(Entrust::hasRole('admin') || (Entrust::hasRole('moderator')))) // Checks the current user
+
+    public static function isMod()
+    {
+        if ( !(Entrust::hasRole('admin') || (Entrust::hasRole('moderator'))) ) // Checks the current user
         {
             return false;
         }
+
         return true;
     }
 
-    public static  function isAdmin() {
-        if (!(Entrust::hasRole('admin'))) // Checks the current user
+    public static function isAdmin()
+    {
+        if ( !(Entrust::hasRole('admin')) ) // Checks the current user
         {
             return false;
         }
+
         return true;
     }
 
-    public static  function isOwner($userId) {
-        if (Auth::user()) {
-            if (Auth::user()->getAuthIdentifier() === $userId) {
+    public static function isOwner($userId)
+    {
+        if ( Auth::user() ) {
+            if ( Auth::user()->getAuthIdentifier() === $userId ) {
                 return true;
             }
+
             return false;
         }
+
         return false;
     }
 }
